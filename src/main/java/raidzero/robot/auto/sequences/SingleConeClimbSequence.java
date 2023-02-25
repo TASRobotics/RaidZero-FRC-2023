498a0976919395dda78978db90261ea6bcbd1d0e
Jason <24jasonz@students.tas.tw>
2023-02-25 18:15:46 +0800

    auton working

--- src/main/java/raidzero/robot/auto/sequences/SingleConeClimbSequence.java
+++ src/main/java/raidzero/robot/auto/sequences/SingleConeClimbSequence.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import raidzero.robot.Constants.ArmConstants;
 import raidzero.robot.Constants.SwerveConstants;
 import raidzero.robot.auto.actions.ArmHomeAction;
@@ -15,38 +16,37 @@ import raidzero.robot.auto.actions.RunIntakeAction;
 import raidzero.robot.auto.actions.SeriesAction;
 import raidzero.robot.auto.actions.WaitAction;
 import raidzero.robot.submodules.Intake;
+import raidzero.robot.submodules.Swerve;
 
 public class SingleConeClimbSequence extends AutoSequence {
     private final static Intake mIntake = Intake.getInstance();
+    private static final Swerve mSwerve = Swerve.getInstance();
 
-    private PathPlannerTrajectory mTrajectory = PathPlanner.loadPath("SCC Balance",
-            SwerveConstants.MAX_DRIVE_VEL_MPS * 0.75,
-            SwerveConstants.MAX_DRIVE_ACCEL_MPSPS, false);
-    private PathPlannerTrajectory mOverRamp = PathPlanner.loadPath("TCC Over", SwerveConstants.MAX_DRIVE_VEL_MPS,
-            SwerveConstants.MAX_DRIVE_ACCEL_MPSPS);
-    private PathPlannerTrajectory mBalance = PathPlanner.loadPath("TCC Balance", SwerveConstants.MAX_DRIVE_VEL_MPS,
-            SwerveConstants.MAX_DRIVE_ACCEL_MPSPS);
+    private PathPlannerTrajectory mOverRamp = PathPlanner.loadPath("TCC Over", SwerveConstants.MAX_DRIVE_VEL_MPS * 0.5,
+            SwerveConstants.MAX_DRIVE_ACCEL_MPSPS * 0.5);
+    private PathPlannerTrajectory mBalance = PathPlanner.loadPath("TCC Balance", SwerveConstants.MAX_DRIVE_VEL_MPS * 0.5,
+            SwerveConstants.MAX_DRIVE_ACCEL_MPSPS * 0.5);
 
     public SingleConeClimbSequence() {
+        PathPlannerTrajectory.transformTrajectoryForAlliance(mOverRamp, DriverStation.getAlliance());
+        PathPlannerTrajectory.transformTrajectoryForAlliance(mBalance, DriverStation.getAlliance());
     }
 
     @Override
     public void sequence() {
         addAction(
-                new SeriesAction(Arrays.asList(
-                        new RunIntakeAction(0.2, 0.5),
-                        new MoveTwoPronged(-ArmConstants.INTER_GRID_HIGH[0], ArmConstants.INTER_GRID_HIGH[1], 70, -ArmConstants.GRID_HIGH[0], ArmConstants.GRID_HIGH[1], 155),
-                        new RunIntakeAction(1, -1),
-                        new ArmHomeAction(),
-                        new DrivePath(mOverRamp),
-                        new WaitAction(1),
-                        new DrivePath(mBalance))));
+            new SeriesAction(Arrays.asList(
+                new RunIntakeAction(0.2, 0.5),
+                new MoveTwoPronged(-ArmConstants.INTER_GRID_HIGH[0], ArmConstants.INTER_GRID_HIGH[1], 70, -ArmConstants.GRID_HIGH[0], ArmConstants.GRID_HIGH[1], 155),
+                new RunIntakeAction(1, -1),
+                new ArmHomeAction(),
+                new DrivePath(mOverRamp),
+                new DrivePath(mBalance), 
+                new LambdaAction(() -> mSwerve.rotorBrake(true)))));
     }
 
     @Override
-    public void onEnded() {
-
-    }
+    public void onEnded() {}
 
     @Override
     public String getName() {
