baa7b4592f46d6f3147f5fb36059952db7816bcc
Rex Liu <rexypad@gmail.com>
2023-04-11 07:56:11 +0800

    5to1 gr intake

--- src/main/java/raidzero/robot/teleop/Teleop.java
+++ src/main/java/raidzero/robot/teleop/Teleop.java
@@ -138,12 +138,12 @@ public class Teleop {
             arm.moveTwoPronged(
                     ArmConstants.INTER_REV_CUBE_FLOOR_INTAKE,
                     ArmConstants.REV_CUBE_FLOOR_INTAKE, false);
-            intake.setPercentSpeed(-0.50);
+            intake.setPercentSpeed(-0.80);
         } else if (arm.isSafe()) {
             fIntake = false;
         } else if (fIntake && !p.getRightBumper() && !p.getLeftBumper()) {
             arm.goHome();
-            intake.setPercentSpeed(-0.50);
+            intake.setPercentSpeed(-0.80);
         }
 
         // double xSpeed = -p.getLeftY() * reverse * (aiming ? arm.tooFasttooFurious() *
@@ -446,7 +446,7 @@ public class Teleop {
         if (p.getRawButton(12)) {
             intake.setPercentSpeed(0.5);
         } else if (p.getRawButton(11)) {
-            intake.setPercentSpeed(-0.5);
+            intake.setPercentSpeed(-0.8);
         } else if (!p1.getLeftBumper() && !p1.getAButton() && !p1.getRightBumper() && !p.getRawButton(12)
                 && !p.getRawButton(11)) {
             intake.holdPosition();
